8e83025bb17c2db6e8c12765afc239de80306bf2
Gergely Laszlo Csepanyi <gergely-laszlo.csepanyi@nixs.com>
2023-03-27 17:48:38 +0200

    Task 1 is finished.

---
+++ Tasks/01/1_Intro.ts
@@ -0,0 +1,44 @@
+/*
+    ████████╗██╗░░░██╗██████╗░███████╗░██████╗░█████╗░██████╗░██╗██████╗░████████╗
+    ╚══██╔══╝╚██╗░██╔╝██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝
+    ░░░██║░░░░╚████╔╝░██████╔╝█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░
+    ░░░██║░░░░░╚██╔╝░░██╔═══╝░██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░
+    ░░░██║░░░░░░██║░░░██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░
+    ░░░╚═╝░░░░░░╚═╝░░░╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░
+    https://www.typescriptlang.org
+
+    Here are a few statements to understand what this TypeScript is.
+
+        TypeScript is a programming language based on JavaScript.
+
+        TypeScript is a strongly typed and compiled language.
+    Although at the output the compiler creates the same JavaScript, which is executed then by the browser.
+    However, strong typing reduces the number of potential errors that might occur when developing with JavaScript.
+
+        TypeScript implements many of the concepts that are native to object-oriented languages, such as:
+    - inheritance, 
+    - polymorphism, 
+    - encapsulation,
+    - access modifiers and so on.
+
+        TypeScript makes it faster and easier to write large complex programs,
+    accordingly, they are easier to maintain, develop, scale and test than in standard JavaScript
+    
+        TypeScript develops as an open source project. 
+    Repository address - https://github.com/Microsoft/TypeScript. 
+    In addition, it is cross-platform, which means that we can use both Windows and MasOS or Linux for development.
+
+        TypeScript is a typed superset of JavaScript, which means that any JS program is a TypeScript program. 
+    In TS, you can use all those constructions that are used in JS - the same operators, conditional, cyclic constructions. 
+
+        How to get started with TypeScript? 
+        You can install TypeScript using npm, the package manager for Node.js (https://nodejs.org/uk/download/). 
+    After installing Node.js from the official site, npm will automatically appear in the system. 
+    
+    You can read more about installing the TypeScript compiler and using it in Visual Studio Code in the documentation.
+/*
+        Task
+    1) Install TypeScript globally using the command in the console: npm install -g typescript
+    2) Check out the article at the link https://code.visualstudio.com/docs/typescript/typescript-compiling.
+    2) Make all the necessary settings and create your first helloword.ts file following the example.
+*/
